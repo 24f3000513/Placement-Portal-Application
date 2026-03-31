cd75b82a167bf7ee74807ea842fe56a18890b05a
24f3000513 <24f3000513@ds.study.iitm.ac.in>
2026-03-31 11:22:19 +0530

    Updates in database and RBAC

--- app.py
+++ app.py
@@ -12,69 +12,72 @@ from models import User
 import os
 
 
-login_manager = LoginManager()
-login_manager.login_view = "auth.login"
-
+l_manager = LoginManager()
+l_manager.login_view = "auth.login"
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 database.init_app(app)
-login_manager.init_app(app)
+l_manager.init_app(app)
 
 app.register_blueprint(auth_bp)
 
 os.makedirs('database',exist_ok=True)
 
 with app.app_context():
-    from models import User,Student,Company,Drive,Application
+    from models import User, Student, Company, Drive, Application
     database.create_all()
 
-    admin = User.query.filter_by(roles = "admin").first()
-    if not admin:
-        admin = User(
-            name = "Admin",
-            email = "admin@cistm.ac.in",
-            roles = "admin"
-        )
-        admin.pasw_hash("admin123")
-
-        database.session.add(admin)
-        database.session.commit()
-    else:
-        print("Error")
+    try:
+        chk_admin = User.query.filter_by(roles="admin", email="admin@cistm.ac.in").first()
+        if not chk_admin:
+            admin = User(name="Admin",
+                        email="admin@cistm.ac.in",
+                        roles="admin")
+            admin.pasw_hash("Admin@123")
+            database.session.add(admin)
+            database.session.commit()
+            print("Admin created successfully")
+    except Exception as e:
+        print(f"Error: {e}")
 
 @app.route("/", methods = ['GET','POST'])
 def home():
     return redirect(url_for('auth.login'))
 
-@login_manager.user_loader
+
+@l_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 @app.route("/register", methods = ['GET','POST'])
 def register():
     return render_template('register.html')
 
+
 @app.route("/admin/dashboard")
 @login_required
 def admin():
     if current_user.roles != "admin":
-        return "Unauthorized", 403 #client error
+        return "Unauthorized", 403
     return render_template('admin.html')
+
 
 @app.route("/student/dashboard")
 @login_required
 def student():
     if current_user.roles != "student":
-        return "Unauthorized", 403 #client error
+        return "Unauthorized", 403 
     return render_template('student.html')
+
 
 @app.route("/company/dashboard")
 @login_required
 def company():
     if current_user.roles != "company":
-        return "Unauthorized", 403 #client error
+        return "Unauthorized", 403 
     return render_template('company.html')
 
 
--- models/users.py
+++ models/users.py
@@ -14,8 +14,28 @@ class User(UserMixin,database.Model):
     roles = database.Column(Enum("student", "company", "admin", name="user_role"),nullable = False)
     is_active = database.Column(database.Boolean, default=True)
 
+    def valid_pasw(self, pasw):
+        if len(pasw) < 8:
+            return False, "Password must be more than 7 characters"
+
+        if not any(c.isupper() for c in pasw):
+            return False, "Password must contain at least one uppercase letter"
+
+        if not any(c.islower() for c in pasw):
+            return False, "Password must contain at least one lowercase letter"
+
+        if not any(c in "!@#$%^&*[](){}" for c in pasw):
+            return False, "Password must contain at least one special character (!@#$%^&*[](){})"
+
+        return True, "Valid password"
+    
     def pasw_hash(self,pasw):
-        self.password = generate_password_hash(pasw)
+        x,msg = self.valid_pasw(pasw)
+        if x :
+            self.password = generate_password_hash(pasw)
+            return True,msg
+        else:
+            return False,msg
 
     def chk_hash_pasw(self,pasw):
         return check_password_hash(self.password,pasw)
